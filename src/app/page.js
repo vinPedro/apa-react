1c03b16d48add4b738bb0ef6adc83c49346fd4e8
Pedro Vinicius <159270159+vinPedro@users.noreply.github.com>
2025-10-26 07:59:15 -0300

    Primeiro commit: commit após a criação do projeto e adição dos recursos iniciais para funcionamento.

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import Botao from "@/components/Botao";
 import Image from "next/image";
 
 export default function Home() {
@@ -50,6 +51,7 @@ export default function Home() {
             Read our docs
           </a>
         </div>
+        <Botao/>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
---
+++ src/components/Botao.jsx
@@ -0,0 +1,8 @@
+
+export default function Botao() {
+    return (
+        <div>
+            <button className="bg-amber-50 w-full text-black p-2" >Button</button>
+        </div>
+    )
+}
