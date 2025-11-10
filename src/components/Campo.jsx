73523d652625a519018b290ac0c25d05ef2a5e53
reysoares <soaresreynaldo240@gmail.com>
2025-11-10 15:13:17 -0300

    validação edestaque de campos obrigatórios no login

--- src/components/Campo.jsx
+++ src/components/Campo.jsx
@@ -1,9 +1,14 @@
-export default function Campo({ label, type = "text", value, onChange, autoComplete = "on", name, disabled, placeholder }) {
+export default function Campo({ label, type = "text", value, onChange, autoComplete = "on", name, disabled, placeholder, error }) {
+  
+  const errorClass = error ? 'border-red-500' : 'border-foreground';
+
+  const baseClasses = "border rounded-sm pt-[clamp(1.5px,2vw,3px)] pb-[clamp(2.5px,2vw,4.5px)] pl-2 min-w-[300px] max-w-[700px] w-full";
+
   return (
     <div>
       <label className="text-foreground">{label}</label><br />
-      <input className="border rounded-sm border-foreground pt-[clamp(1.5px,2vw,3px)] pb-[clamp(2.5px,2vw,4.5px)] pl-2 min-w-[300px] max-w-[700px] w-full"
-
+      <input 
+        className={`${baseClasses} ${errorClass}`} // Combina as classes base com a classe de erro/sucesso
         name={name}
         autoComplete={autoComplete}
         type={type}
@@ -12,6 +17,8 @@ export default function Campo({ label, type = "text", value, onChange, autoCompl
         disabled={disabled}
         placeholder={placeholder}
       />
+      
+      {error && <span className="text-red-500 text-sm">{error}</span>}
     </div>
   );
 }
